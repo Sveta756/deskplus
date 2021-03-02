4efe55cd7b3977d7bb8bdc5729b57cc0f72217b8
Sveta <paranyushkina.sv@gmail.com>
2021-03-02 14:15:10 +0300

    video work

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,24 @@
 const btns = document.querySelectorAll('.btn');
 const overlay = document.querySelector('.overlay');
-const modalClose = document.querySelector('.modal__close');
+const modalClose = document.querySelector('.close');
 const trigger = document.querySelector('.video__play');
+const tag = document.createElement('script');
 
-  btns.forEach(elem => {
+tag.src = "https://www.youtube.com/iframe_api"; //работа ютуб апи для видео
+const firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+let player;  //создание плеера
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: '100%',
+      width: '100%',
+      videoId: 'kSUxuo8WvGI'
+    });
+  }
+
+  btns.forEach(elem => {  //свиталерт на кнопки
       elem.addEventListener('click',(e) => {
         e.preventDefault();
           Swal.fire(
@@ -14,20 +29,26 @@ const trigger = document.querySelector('.video__play');
       });
   });
 
-  trigger.addEventListener('click', () => {
+
+  //работа модального окна с видео 
+
+  trigger.addEventListener('click', () => { 
     overlay.classList.add('block');
+    player.playVideo();
     document.body.style.overflow = 'hidden';
   });
 
   modalClose.addEventListener('click', () => {
+    player.stopVideo();
     overlay.classList.remove('block');
     document.body.style.overflow = '';
   });
 
   overlay.addEventListener('click', (e) => {  //клик по фону, но не модальное окно
     if (e.target === overlay) {
+      player.stopVideo();
       overlay.classList.remove('block');
       document.body.style.overflow = '';
     }
-});
-  
+  });
+
